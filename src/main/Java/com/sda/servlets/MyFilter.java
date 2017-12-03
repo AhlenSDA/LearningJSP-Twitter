d53e2239a2d7285e8e726ae9ff0182d66ce71789
AhlenSDA <ahlensda@wp.pl>
2017-12-03 14:18:55 +0100

    Debugging, filterChain

---
+++ src/main/Java/com/sda/servlets/MyFilter.java
@@ -0,0 +1,27 @@
+package com.sda.servlets;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(filterName = "myFilter", servletNames = {"firstServlet"})
+public class MyFilter implements Filter {
+
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    public void destroy() {
+
+    }
+}
